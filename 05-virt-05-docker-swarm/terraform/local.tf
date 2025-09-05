355082467e6aa6913c7e43e13d09b083dbae45a8
DegtyarevAO <Degtjarew@yandex.ru>
2025-09-05 22:49:46 +0300

    Create local.tf

---
+++ 05-virt-05-docker-swarm/terraform/local.tf
@@ -0,0 +1,4 @@
+locals {
+  vm_name_worker1 = "${var.project}-${var.contur}-${var.role1}"
+  vm_name_worker2 = "${var.project}-${var.contur}-${var.role2}"
+  vm_name_arbitr = "${var.project}-${var.contur}-${var.role3}"
